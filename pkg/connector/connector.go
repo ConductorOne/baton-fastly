7101da760e70659f196aca74a608834a7e2d81e3
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2023-10-19 23:43:09 +0200

    feat: connector

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -7,12 +7,17 @@ import (
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/fastly/go-fastly/v8/fastly"
 )
 
-type Connector struct{}
+type Fastly struct {
+	client *fastly.Client
+
+	customerId string
+}
 
 // ResourceSyncers returns a ResourceSyncer for each resource type that should be synced from the upstream service.
-func (d *Connector) ResourceSyncers(ctx context.Context) []connectorbuilder.ResourceSyncer {
+func (d *Fastly) ResourceSyncers(ctx context.Context) []connectorbuilder.ResourceSyncer {
 	return []connectorbuilder.ResourceSyncer{
 		newUserBuilder(),
 	}
@@ -20,25 +25,38 @@ func (d *Connector) ResourceSyncers(ctx context.Context) []connectorbuilder.Reso
 
 // Asset takes an input AssetRef and attempts to fetch it using the connector's authenticated http client
 // It streams a response, always starting with a metadata object, following by chunked payloads for the asset.
-func (d *Connector) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCloser, error) {
+func (d *Fastly) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCloser, error) {
 	return "", nil, nil
 }
 
 // Metadata returns metadata about the connector.
-func (d *Connector) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
+func (d *Fastly) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
 	return &v2.ConnectorMetadata{
-		DisplayName: "My Baton Connector",
-		Description: "The template implementation of a baton connector",
+		DisplayName: "Fastly",
+		Description: "Connector syncing Fastly resources to Baton",
 	}, nil
 }
 
 // Validate is called to ensure that the connector is properly configured. It should exercise any API credentials
 // to be sure that they are valid.
-func (d *Connector) Validate(ctx context.Context) (annotations.Annotations, error) {
+func (d *Fastly) Validate(ctx context.Context) (annotations.Annotations, error) {
 	return nil, nil
 }
 
 // New returns a new instance of the connector.
-func New(ctx context.Context) (*Connector, error) {
-	return &Connector{}, nil
+func New(ctx context.Context, accessToken string) (*Fastly, error) {
+	client, err := fastly.NewClient(accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := client.GetCurrentUser()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Fastly{
+		client:     client,
+		customerId: user.CustomerID,
+	}, nil
 }
