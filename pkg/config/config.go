cf4cfdc0564cc19bb8f800beece09b2cf9ff6eab
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:27:20 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2
    - Migrate from cli.NewCmd to config.DefineConfiguration pattern
    - Add WithDefaultCapabilitiesConnectorBuilder for capabilities command
    - Update CI workflows to use go-version-file from go.mod
    - Update golangci-lint config to v2 format
    - Add tools/rules.go for ruleguard integration

---
+++ pkg/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/conductorone/baton-sdk/pkg/field"
+)
+
+var (
+	AccessToken = field.StringField(
+		"access-token",
+		field.WithDescription("Fastly API token"),
+		field.WithRequired(true),
+		field.WithIsSecret(true),
+		field.WithDisplayName("Access Token"),
+	)
+
+	// FieldRelationships defines relationships between the fields listed in
+	// Config that can be automatically validated.
+	FieldRelationships = []field.SchemaFieldRelationship{}
+)
+
+//go:generate go run ./gen
+var Config = field.NewConfiguration([]field.SchemaField{
+	AccessToken,
+})
+
+// ValidateConfig is run after the configuration is loaded, and should return an
+// error if it isn't valid. Implementing this function is optional, it only
+// needs to perform extra validations that cannot be encoded with configuration
+// parameters.
+func ValidateConfig(cfg *Fastly) error {
+	return nil
+}
