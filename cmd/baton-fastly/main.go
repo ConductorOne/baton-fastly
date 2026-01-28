19d7dafdb442562dab39cef27ea604a7baa3d57b
Steve Gontzes <gontzess@gmail.com>
2026-01-28 14:22:19 -0500

    Upgrade baton-sdk to v0.7.x and Go to 1.25.x (#8)
    * Upgrade baton-sdk to v0.7.4 and Go to 1.25.2
    
    - Migrate from cli.NewCmd to config.DefineConfiguration pattern
    - Add WithDefaultCapabilitiesConnectorBuilder for capabilities command
    - Update CI workflows to use go-version-file from go.mod
    - Update golangci-lint config to v2 format
    - Add tools/rules.go for ruleguard integration
    
    * chore: add config generation to Makefile
    
    Add GENERATED_CONF variable and make build depend on generated config.
    Add generate target for explicit config regeneration.
    
    * chore: add BUILD_TAGS support to Makefile
    
    Add conditional build tag for lambda support to match baton-datadog pattern.

--- cmd/baton-fastly/main.go
+++ cmd/baton-fastly/main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	cfg "github.com/conductorone/baton-fastly/pkg/config"
+	"github.com/conductorone/baton-fastly/pkg/connector"
+	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
-
-	"github.com/conductorone/baton-fastly/pkg/connector"
 )
 
 var version = "dev"
@@ -19,15 +20,19 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	cfg := &config{}
-	cmd, err := cli.NewCmd(ctx, "baton-fastly", cfg, validateConfig, getConnector)
+	_, cmd, err := config.DefineConfiguration(
+		ctx,
+		"baton-fastly",
+		getConnector,
+		cfg.Config,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Fastly{}),
+	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	cmd.Version = version
-	cmdFlags(cmd)
 
 	err = cmd.Execute()
 	if err != nil {
@@ -36,10 +41,13 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, cc *cfg.Fastly) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
+	if err := cfg.ValidateConfig(cc); err != nil {
+		return nil, err
+	}
 
-	cb, err := connector.New(ctx, cfg.AccessToken)
+	cb, err := connector.New(ctx, cc.AccessToken)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
