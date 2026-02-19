7f8cf52e65b93491067c7f95abaa9bf5aa791743
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:03:00 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,6 +16,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the Fastly API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	// FieldRelationships defines relationships between the fields listed in
